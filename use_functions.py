17bc96dbc3dbe7e84d29d200bab88f4e1a335936
alexbsdn <alexbsdn@gmail.com>
2022-01-16 21:52:54 +0500

    Module 3 done

--- use_functions.py
+++ use_functions.py
@@ -33,20 +33,40 @@
 
 Для реализации основного меню можно использовать пример ниже или написать свой
 """
+def credit(sum):
+    sum += int(input('Введите сумму пополнения счёта: '))
+    return sum
 
+def purchase(sum, lst):
+    price = int(input('Введите цену товара: '))
+    if price > sum:
+        print('Недостаточно денег на счёте!')
+    else:
+        good = input('Введите название товара: ')
+        lst.append([good, price])
+        sum -= price
+    return sum, lst
+
+def history(lst):
+    print('История покупок', lst)
+
+account = 0
+goods = []
 while True:
     print('1. пополнение счета')
     print('2. покупка')
     print('3. история покупок')
     print('4. выход')
 
-    choice = input('Выберите пункт меню')
+    choice = input('Выберите пункт меню: ')
     if choice == '1':
-        pass
+        account = credit(account)
+        print('Сумма на счёте = ', account)
     elif choice == '2':
-        pass
+        account, goods = purchase(account, goods)
+        print('Сумма на счёте = ', account)
     elif choice == '3':
-        pass
+        history(goods)
     elif choice == '4':
         break
     else:
